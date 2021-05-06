ba918345c1e6ab0f36962a1630b2970bf27ca976
Iffaazzahraf <80144840+Iffaazzahraf@users.noreply.github.com>
2021-05-06 13:03:33 +0700

    Add files via upload

---
+++ I0320050_soal 1_tugas 10.py
@@ -0,0 +1,9 @@
+#mkdir
+import os
+
+def main():
+    os.mkdir("ive cantik bgt")
+
+ifname= "main"
+
+main()
---
+++ I0320050_soal 4_tugas 10.py
@@ -0,0 +1,11 @@
+
+#rename
+import os
+
+def main():
+    os.rename("ive cantik bgt" ,"Iffa azzahra_I0320050")
+
+ifname= "main"
+
+main()
+
